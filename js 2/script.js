cb2dbb2c8f4abc69f9cfc15bfa86198ccfa7fb2d
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-20 18:37:54 +0100

    [FIX] aggiustati errori

--- js 2/script.js
+++ js 2/script.js
@@ -1,33 +1,32 @@
 // SECONDO ESERCIZIO: DADI
 
-let numberUtente = prompt('Inserisci un numero da 1 a 6')
-console.log(numberUtente + ' ' + "Numero dell'utente")
+let numberUtente = prompt('Inserisci un numero da 1 a 6');
+console.log(numberUtente + ' ' + "Numero dell'utente");
 
-let numberComputer = Math.random();
-console.log(Math.floor(Math.random() * 6) + 1 + ' ' + "Numero del computer")
+let numberComputer = Math.floor(Math.random() * 6) + 1;
+console.log(Math.floor(Math.random() * 6) + 1 + ' ' + "Numero del computer");
 
 let vittoria = ['Vittoria', 'Sconfitta', 'Pareggio'];
+
 if (numberUtente >= 1 && numberUtente <= 6) {
 
-    if (numberUtente > Math.floor(Math.random() * 6) + 1) {
+    if (numberUtente > numberComputer) {
         vittoria = 'Vittoria';
-        console.log(vittoria + ' ' + ': l\'utente ha VINTO!')
+        console.log(vittoria + ' ' + ': l\'utente ha VINTO!');
     }
 
-    else if (numberUtente < Math.floor(Math.random() * 6) + 1) {
+    else if (numberUtente < numberComputer ) {
         vittoria = 'Sconfitta';
-        console.log(vittoria + ' ' + ': Il computer ha VINTO!')
+        console.log(vittoria + ' ' + ': Il computer ha VINTO!');
     }
-    else
-    //if (numberUtente === Math.floor(Math.random() * 6) + 1)
-    {
+    else if (numberUtente == numberComputer) {
         vittoria = 'Pareggio';
         console.log(vittoria);
     }
 }
 
 else {
-    console.log('ERRORE: Hai inserito un numero maggiore di 6, Ritenta!')
+    console.log('ERRORE: Hai inserito un numero maggiore di 6, Ritenta!');
 }
 
 
