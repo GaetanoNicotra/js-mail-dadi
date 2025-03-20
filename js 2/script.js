57c74d788bc1845fdaf0492f7ea8f181dd734788
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-20 17:21:15 +0100

    [UPD] Scritta la struttura del codice

--- js 2/script.js
+++ js 2/script.js
@@ -1,7 +1,18 @@
 // SECONDO ESERCIZIO: DADI
 
 let numberUtente = prompt('Inserisci un numero da 1 a 6')
-console.log(numberUtente)
+console.log(numberUtente + ' ' + "Numero dell'utente")
 
 let numberComputer = Math.random();
-console.log( Math.floor(Math.random()*6)+1)
+console.log(Math.floor(Math.random() * 6) + 1 + ' ' + "Numero del computer")
+
+if (numberUtente > numberComputer) {
+    console.log('L\'utente ha vinto')
+}
+
+else if (numberUtente < numberComputer) {
+    console.log('Il computer ha vinto')
+}
+else{
+    console.log('Pareggio')
+}
