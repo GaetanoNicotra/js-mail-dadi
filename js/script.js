b5bba75786a50349679865ac0ab5208b77479308
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-20 16:13:05 +0100

    [UPD] Primo esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// MAIL
+
+// creo l'array
+
+const emailList = ['luca@gmail.com', 'marco@gmail.com', 'gaetano@gmail.com', 'filippo@gmail.com', 'antonio@gmail.com', 'andrea@gmail.com']
+
+let tuaMail = prompt('Inserisci la tua email');
+
+let presente = false;
+
+for (let i=0; !presente && i<=emailList.length; i++){
+    if( emailList[i] === tuaMail){
+        presente = true;
+    }
+}
+
+if(presente === true){
+    console.log('Sei presente nella lista, Puoi entrare!')
+}
+else{
+    console.log('Non sei presente nella lista!')
+}
