a905305c170636285ae5428611e6c794fa3495ae
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-20 19:18:37 +0100

    [FIX]

--- js 2/script.js
+++ js 2/script.js
@@ -10,29 +10,35 @@ let vittoria = ['Vittoria', 'Sconfitta', 'Pareggio'];
 
 if (numberUtente >= 1 && numberUtente <= 6) {
 
-    if (numberUtente > numberComputer) {
-        vittoria = 'Vittoria';
-        console.log(vittoria + ' ' + ': l\'utente ha VINTO!');
-    }
-
-    else if (numberUtente < numberComputer ) {
-        vittoria = 'Sconfitta';
-        console.log(vittoria + ' ' + ': Il computer ha VINTO!');
-    }
-    else if (numberUtente == numberComputer) {
-        vittoria = 'Pareggio';
-        console.log(vittoria);
-    }
 }
 
 else {
+    console.log(alert('ERRORE: Hai inserito un numero maggiore di 6, Ritenta!'));
     console.log('ERRORE: Hai inserito un numero maggiore di 6, Ritenta!');
 }
 
+if (numberUtente > numberComputer) {
+    vittoria = 'Vittoria';
+    console.log(vittoria + ' ' + ': l\'utente ha VINTO!');
+}
 
-/*if(vittoria === 'vittoria'){
+else if (numberUtente < numberComputer) {
+    vittoria = 'Sconfitta';
+    console.log(vittoria + ' ' + ': Il computer ha VINTO!');
+}
+else {
+    vittoria = 'Pareggio';
+    console.log(vittoria);
+}
+
+
+/*if (vittoria == 'Vittoria') {
     console.log('L\'utente ha vinto');
 }
-else{
-     console.log('Il computer ha vinto')
+else if (vittoria == 'Sconfitta') {
+    console.log('Il computer ha vinto')
+}
+
+else if ((vittoria == 'Pareggio')) {
+    console.log('P')
 }*/
