1a128e3a19ec83d54af3fdb90ca0578515f5da03
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-20 19:43:15 +0100

    [FIX] Correzione errori

--- js 2/script.js
+++ js 2/script.js
@@ -4,17 +4,17 @@ let numberUtente = prompt('Inserisci un numero da 1 a 6');
 console.log(numberUtente + ' ' + "Numero dell'utente");
 
 let numberComputer = Math.floor(Math.random() * 6) + 1;
-console.log(Math.floor(Math.random() * 6) + 1 + ' ' + "Numero del computer");
+console.log(numberComputer + ' ' + "Numero del computer");
 
-let vittoria = ['Vittoria', 'Sconfitta', 'Pareggio'];
+let vittoria;
 
 if (numberUtente >= 1 && numberUtente <= 6) {
 
 }
 
 else {
-    console.log(alert('ERRORE: Hai inserito un numero maggiore di 6, Ritenta!'));
-    console.log('ERRORE: Hai inserito un numero maggiore di 6, Ritenta!');
+    alert('ERRORE: Devi inserire un numero compreso tra 1 e 6, Ritenta!');
+    console.log('ERRORE: Devi inserire un numero compreso tra 1 e 6, Ritenta!');
 }
 
 if (numberUtente > numberComputer) {
@@ -30,15 +30,3 @@ else {
     vittoria = 'Pareggio';
     console.log(vittoria);
 }
-
-
-/*if (vittoria == 'Vittoria') {
-    console.log('L\'utente ha vinto');
-}
-else if (vittoria == 'Sconfitta') {
-    console.log('Il computer ha vinto')
-}
-
-else if ((vittoria == 'Pareggio')) {
-    console.log('P')
-}*/
